dafbd10bbf8b3b171a615d7d38b6fefaf5585dab
Carlos Eduardo <carloseduardomveloso@gmail.com>
2024-09-06 09:43:41 -0300

    prototipo inicial

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,29 @@
 function pesquisar(){
-    let section = document.getElementById("resultados-pesquisa")
+    let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você não buscou nada sobre linguagens de progrmação</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    
     //Inicializa um String vazia para armazenar os Dados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
     
     //para cada dado dentro da lista de dados
     //Itera sobre cada dado da lista de dados
     for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        //se titulo includes campoPesquisa entao, faça...
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
         //Cria um novo elemento HTML para cada resultado
         resultados += `
         <div class="item-resultado">
@@ -14,8 +31,14 @@ function pesquisar(){
                         <p class="descricao-meta">${dado.descricao}</p>
                         <a href="${dado.link}" target="_blank">Aprender mais !</a>
                     </div>
-        `
+        `;
+    }
     };
+
+    if (!resultados) {
+       resultados = "<p>Encontramos nada relacionado</p>"
+    }
+
     //Atribui os resultados gerados à seção HTML 
     section.innerHTML = resultados  
 }
