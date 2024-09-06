dc368defd40f0ffa85e8951cbfcdbc9a1424bd18
Carlos Eduardo <carloseduardomveloso@gmail.com>
2024-09-05 23:23:22 -0300

    atualização

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-console.log(dados);   
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa")
+
+    //Inicializa um String vazia para armazenar os Dados
+    let resultados = "";
+    
+    //para cada dado dentro da lista de dados
+    //Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        //Cria um novo elemento HTML para cada resultado
+        resultados += `
+        <div class="item-resultado">
+                        <h2>${dado.titulo}</h2>
+                        <p class="descricao-meta">${dado.descricao}</p>
+                        <a href="${dado.link}" target="_blank">Aprender mais !</a>
+                    </div>
+        `
+    };
+    //Atribui os resultados gerados à seção HTML 
+    section.innerHTML = resultados  
+}
+
+
+
+
+
+
